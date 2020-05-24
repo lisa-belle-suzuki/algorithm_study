46c7c9b2b258efaaaa100268d9a756a3edf0c65f
lisa-belle-suzuki <lisamarines_bb4@icloud.com>
2020-05-24 16:14:44 +0900

    Educational DP G (2回目) (AC)

--- atcoder/educationalDP-G.cpp
+++ atcoder/educationalDP-G.cpp
@@ -5,24 +5,26 @@ using namespace std;
 bool visited[100002];
 int dp[100002];
 
-bool graph[100002][100002];
+vector<int> graph[100002];
 
 int N, M ,x, y;
 
 int solve(int i){
     if (visited[i]) return dp[i];
     int longest = 0;
-    for(int j=1; j<=N; j++){
-        if (graph[i][j]==true){
-            if (visited[j]==true){
-                if (longest < dp[j] + 1) longest = dp[j];
-            }else{
-                int candid = solve(j);
-                if (longest < candid + 1) longest = candid + 1;
-            }
+    vector<int> connected_list = graph[i];
+    // cout  << "i = " << i << ": connected_list.size() = " << connected_list.size() << endl;
+    for(int j=0; j<connected_list.size(); j++){
+        int connected = connected_list[j];
+        if (visited[connected]==true){
+            if (longest < dp[connected] + 1) longest = dp[connected] + 1;
+        }else{
+            int candid = solve(connected);
+            if (longest < candid + 1) longest = candid + 1;
         }
     }
     dp[i] = longest;
+    visited[i] = true;
     return longest;
 }
 
@@ -31,7 +33,10 @@ int main(){
     cin >> N >> M;
     for(int i=1; i<=M; i++){
         cin >> x >> y;
-        graph[x][y] = true;
+        graph[x].push_back(y);
+    }
+    for(int i=1; i<=N; i++){
+        visited[i] = false;
     }
 
     int longest = 0, candid;
@@ -40,6 +45,7 @@ int main(){
         // cout << "i = " << i << ": candid = " << candid << endl;
         if (candid > longest) longest = candid;
     }
+
     cout << longest << endl;
     return 0;
 }
