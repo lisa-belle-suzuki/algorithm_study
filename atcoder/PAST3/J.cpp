37a1ad34f01113699dc2904974ef84ede9702554
lisa-belle-suzuki <lisamarines_bb4@icloud.com>
2020-06-07 04:54:44 +0900

    PAST第3回 J.cpp(途中2)

--- atcoder/PAST3/J.cpp
+++ atcoder/PAST3/J.cpp
@@ -9,36 +9,42 @@ bool hasEaten[100002];
 int maxYummy[100002];
 
 
-bool solve(int i, int j){
-    if (!hasEaten[j]){
-        cout << j << endl;
-        hasEaten[j] = true;
-        maxYummy[j] = a[i];
-        return true;
-    }else if (a[i] > maxYummy[j]){
-        cout << j << endl;
-        maxYummy[j] = a[i];
-        return true;
-    }
-    return false;
-}
-
 int main(){
     int N, M;
     cin >> N >> M;
     for(int i=0; i<M; i++){
         cin >> a[i];
     }
-
-    for(int i=0; i<M; i++){
-        bool hasSomeoneEaten = false;
-        for(int j=1; j<=N; j++){
-            if (solve(i, j)){
-                hasSomeoneEaten = true;
+    //最初の1個は1番が食べる
+    cout << 1 << endl;
+    hasEaten[1] = true;
+    maxYummy[1] = a[0];
+    // 残りの寿司について解く
+    int start_j = 1;
+    for(int i=1; i<M; i++){
+        if (a[i-1] < a[i]){
+            start_j = 1;
+        }
+        bool isSomeoneAte = false;
+        for(int j=start_j; j<=N; j++){
+            if (hasEaten[j]==false){
+                cout << j << endl;
+                isSomeoneAte = true;
+                hasEaten[j] = true;
+                maxYummy[j] = a[i];
+                start_j = j+1;
+                break;
+            }else if (a[i] > maxYummy[j]){
+                cout << j << endl;
+                // cout << "original maxYummy = " << maxYummy[j];
+                isSomeoneAte = true;
+                maxYummy[j] = a[i];
+                // cout << ", new maxYummy = " << maxYummy[j] << endl;
+                start_j = j+1;
                 break;
             }
         }
-        if (hasSomeoneEaten==false) cout << -1 << endl;
+        if (isSomeoneAte==false) cout << -1 << endl;
     }
 
     return 0;
