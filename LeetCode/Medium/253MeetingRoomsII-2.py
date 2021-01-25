756a4c6b742d4abd085445fce525ad503ebe0fa7
lisa-belle-suzuki <lisamarines_bb4@icloud.com>
2021-01-25 18:02:48 +0900

    LeetCode Medium/253MeetingRoomsII-2.py (priority queueを使う解法)

---
+++ LeetCode/Medium/253MeetingRoomsII-2.py
@@ -0,0 +1,15 @@
+import heapq
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x:x[0]) 
+
+        rooms = []
+        heapq.heappush(rooms, intervals[0][1])
+
+        for interval in intervals[1:]:
+            if interval[0] >= rooms[0]:
+                heapq.heappop(rooms)
+            heapq.heappush(rooms, interval[1])
+
+        return len(rooms)
