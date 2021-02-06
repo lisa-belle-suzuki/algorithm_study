3805ebbcf30afef69c81d192abaa53b0db36d8fd
lisa-belle-suzuki <lisamarines_bb4@icloud.com>
2021-02-06 18:30:17 +0900

    LeetCode Medium/105ConstructBinaryTreefromPreorderandInorderTraversal-2.py (解説AC)

---
+++ LeetCode/Medium/105ConstructBinaryTreefromPreorderandInorderTraversal-2.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        def solve(in_left = 0, in_right = len(inorder)):
+            nonlocal pre_idx
+            if in_left == in_right:
+                return None
+            root_val = preorder[pre_idx]
+            root = TreeNode(root_val)
+            index = idx_map[root_val]
+            pre_idx += 1
+            root.left = solve(in_left, index)
+            root.right = solve(index + 1, in_right)
+            return root
+
+        pre_idx = 0
+        idx_map = {val:idx for idx,val in enumerate(inorder)}
+        return solve()
