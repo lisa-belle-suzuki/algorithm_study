dc38c2f3b4b865a6d77ea4071a7dc6352cbe32ab
lisa-belle-suzuki <lisamarines_bb4@icloud.com>
2021-02-19 20:44:52 +0900

    LeetCode Medium/1381DesignaStackWithIncrementOperation.py

---
+++ LeetCode/Medium/1381DesignaStackWithIncrementOperation.py
@@ -0,0 +1,27 @@
+class CustomStack:
+
+    def __init__(self, maxSize: int):
+        self.maxSize = maxSize
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        if len(self.stack)>=self.maxSize:
+            return
+        self.stack.append(x)
+        return
+
+    def pop(self) -> int:
+        if len(self.stack)==0: return -1
+        return self.stack.pop()
+
+    def increment(self, k: int, val: int) -> None:
+        for i in range(min(k, len(self.stack))):
+            self.stack[i] += val
+        return
+
+
+# Your CustomStack object will be instantiated and called as such:
+# obj = CustomStack(maxSize)
+# obj.push(x)
+# param_2 = obj.pop()
+# obj.increment(k,val)
