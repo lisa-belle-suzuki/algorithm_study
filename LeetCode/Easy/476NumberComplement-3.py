d53f32ed70349b022d5c8f716169677bdc9819e5
lisa-belle-suzuki <lisamarines_bb4@icloud.com>
2021-02-21 22:21:01 +0900

    LeetCode Easy/476NumberComplement-3.py (OpenJDKアルゴリズムによるビットマスク作成)

---
+++ LeetCode/Easy/476NumberComplement-3.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        bitmask = num
+        bitmask |= (bitmask >> 1)
+        bitmask |= (bitmask >> 2)
+        bitmask |= (bitmask >> 4)
+        bitmask |= (bitmask >> 8)
+        bitmask |= (bitmask >> 16)
+        return bitmask ^ num
