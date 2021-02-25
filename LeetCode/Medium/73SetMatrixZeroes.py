737f814fc513f7cca1912016f76a67d3c35dba7d
lisa-belle-suzuki <lisamarines_bb4@icloud.com>
2021-02-25 11:34:36 +0900

    LeetCode Medium/73SetMatrixZeroes.py

---
+++ LeetCode/Medium/73SetMatrixZeroes.py
@@ -0,0 +1,17 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        row_set = set()
+        col_set = set()
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                if matrix[row][col] == 0:
+                    row_set.add(row)
+                    col_set.add(col)
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                if row in row_set or col in col_set:
+                    matrix[row][col] = 0
+        return
