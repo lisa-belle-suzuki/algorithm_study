23db57811e246b9e5597477b13f1845567ef9b03
lisa-belle-suzuki <lisamarines_bb4@icloud.com>
2021-02-28 22:50:07 +0900

    LeetCode Medium/384ShuffleanArray.py

---
+++ LeetCode/Medium/384ShuffleanArray.py
@@ -0,0 +1,23 @@
+import random
+
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.original = nums
+
+    def reset(self) -> List[int]:
+        """
+        Resets the array to its original configuration and return it.
+        """
+        return self.original
+
+    def shuffle(self) -> List[int]:
+        """
+        Returns a random shuffling of the array.
+        """
+        return random.sample(self.original, len(self.original))
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
